3ee141260c77b63034c0b685b3c211047869ea90
raffaeleav <raff.aviello@gmail.com>
2024-04-25 17:27:37 +0200

    chore: modificati i path per renderli os-independent

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import pandas as pd
 
@@ -6,22 +7,23 @@ from pathlib import Path
 
 
 # funzione per costruire il dataset
-def build_df(dataset_path, output_dir):   
+def build_df(fake_dataset_path, real_dataset_path, output_dir):   
     # dataframe dei metadati del dataset di immagini
-    df = pd.read_csv(dataset_path)
+    df_fake = pd.read_csv(fake_dataset_path)
+    # il dataset delle immagini real è coco
+    df_real = pd.read_csv(real_dataset_path)
 
     # ottengo le immagini anchor, positive e fake
-    df_real = (df[df.target == 0]).sample(2000)
+    df_real = (df_real[df_real.target == 0]).sample(2000)
 
     df_anchor = df_real.head(1000)
     df_positive = df_real.tail(1000)
-    df_negative = (df[df.target != 0]).sample(1000)
+    df_negative = (df_fake[df_fake.target != 0]).sample(1000)
 
     # dataframe finale
     df_out = pd.DataFrame(columns=['Anchor', 'Positive', 'Negative'])
 
     # f(n) = O(n)
-    # non abbastanza info sulla velocita' di .iloc ...
     for i in tqdm(range(1000), desc="building dataframe..."):
         df_out.loc[i] = [
             df_anchor.iloc[i]['image_path'], 
@@ -31,21 +33,36 @@ def build_df(dataset_path, output_dir):
 
     df_out.to_csv(output_dir, index=False)
 
+
 # funzione per controllare che le immagini nel dataset si possano reperire
 def check_image_path():
-    dir_path = r'D:\sviluppo\jupyter-notebooks\fvab\dataset\cycle_gan'
-    img_path = r'st/horse2zebra/img003759.jpg'
-    path = dir_path + r'\\' + img_path
+    # dir dove si trova il dataset artifact
+    path = Path(__file__).parent.parent.parent
+    dataset_path = os.path.join(path, "artifact", "cycle_gan")
+    img_path = os.path.join("st", "horse2zebra", "img003759.jpg")
+    path = os.path.join(dataset_path, img_path)
 
     image = cv2.imread(path)
     cv2.imshow("dataset-image", image)
     cv2.waitKey(0)
 
 
+# attenzione ai path inseriti
+def main():
+    path = Path(__file__).parent.parent.parent
+    project_path = Path(__file__).parent.parent
+
+    artifact_path = os.path.join(path, "artifact")
+    fake_dataset_path = os.path.join(artifact_path, "cycle_gan", "metadata.csv")
+    real_dataset_path = os.path.join(artifact_path, "coco", "metadata.csv")
+
+    output_dir = os.path.join(project_path, "datasets", "out.csv")
+
+    build_df(fake_dataset_path, real_dataset_path, output_dir)
+    
+
+
 if __name__ == "__main__":
-    dataset_path = r"D:\sviluppo\jupyter-notebooks\fvab\dataset\cycle_gan\metadata.csv"
-    output_dir = r"D:\sviluppo\python-workspace\fvab\results\out.csv"
-
-    build_df(dataset_path, output_dir)
-
+    main()
+    
     # check_image_path()
