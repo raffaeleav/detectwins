4215afc4772d71520574332b035107717b89939f
Raffaele Aviello <raffaele@macbook-air.local>
2024-05-10 11:41:26 +0200

    chore: online triplet loss nel nootebook

---
+++ utils/online_hard_mining.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def distance(img_enc, anc_enc_arr):
+    # dist = np.sqrt(np.dot(img_enc-anc_enc_arr, (img_enc- anc_enc_arr).T))
+    dist = np.dot(img_enc-anc_enc_arr, (img_enc-anc_enc_arr).T)
+    # dist = np.sqrt(dist)
+    
+    return dist
+
+
+def online_hard_mining(batch, batch_size):
+    # devo applicare le trasformazioni sulla batch per poi resituirla alla fine
+    A, P, N = batch
+
+    for i in range(batch_size): 
+        print()
+
+    return A, P, N
+        
+
+if __name__ == "__main__": 
+    batch = None
+    
+    online_hard_mining(batch)
