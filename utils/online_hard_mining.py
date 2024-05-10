39ab6c17db03ce47d71551d060504920e853e515
Raffaele Aviello <raffaele@macbook-air.local>
2024-05-10 13:34:08 +0200

    feat: implementazione online_hard_mining()

--- utils/online_hard_mining.py
+++ utils/online_hard_mining.py
@@ -8,21 +8,31 @@ def distance(img_enc, anc_enc_arr):
     return dist
 
 
+# si formano dei triplet seguendo questa relazione: smallest anchor-negative distance, largest anchor-positive distance 
 def online_hard_mining(batch, batch_size):
-    # devo applicare le trasformazioni sulla batch per poi resituirla alla fine
     A, P, N = batch
 
     for i in range(batch_size): 
-        ap_distance = 0
-        an_distance = 0
+        ap_distance = distance(A[i], P[i])
+        an_distance = distance(A[i], N[i])
+
         for j in range(batch_size): 
             temp_ap_dist = distance(A[i], P[j])
             temp_an_dist = distance(A[i], N[j])
+
             if  temp_ap_dist > ap_distance: 
-                print("hit")
+                ap_distance = temp_an_dist
+                temp = P[i]
+                P[i] = P[j]
+                P[j] = P[i]
+            
+            if temp_an_dist < an_distance: 
+                temp = N[i]
+                N[i] = N[j]
+                N[j] = temp
 
     return A, P, N
-        
+
 
 if __name__ == "__main__": 
     batch = None
