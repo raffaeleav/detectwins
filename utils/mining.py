6b130943a4274bee5b142eec8901fe84b7d0c54a
raffaeleav <raff.aviello@gmail.com>
2024-05-23 13:24:40 +0200

    chore: refactoring codice

--- utils/mining.py
+++ utils/mining.py
@@ -1,7 +1,6 @@
 import ast
 import torch
 import numpy as np
-import pandas as pd
 
 
 def distance(x, y):
@@ -48,11 +47,11 @@ def online_hard_mining(A, P, N, batch_size, device):
 # si selezionano i triplet che rispettano questa relazione: f(ap) < f(an) and f(an) < f(ap) + a
 def filter(x, margin):
     # si deserializzano gli array memorizzati precedentemente come stringhe
-    A, P, N = np.array(ast.literal_eval(x["Anchor_embs"])), np.array(ast.literal_eval(x["Positive_embs"])), np.array(ast.literal_eval(x["Negative_embs"]))
-    A, P, N = torch.tensor(A), torch.tensor(P), torch.tensor(N)
+    a, p, n = np.array(ast.literal_eval(x["Anchor_embs"])), np.array(ast.literal_eval(x["Positive_embs"])), np.array(ast.literal_eval(x["Negative_embs"]))
+    a, p, n = torch.tensor(a), torch.tensor(p), torch.tensor(n)
 
-    ap_distance = distance(A, P).detach().numpy()
-    an_distance = distance(A, N).detach().numpy()
+    ap_distance = distance(a, p).detach().numpy()
+    an_distance = distance(a, n).detach().numpy()
 
     return ap_distance < an_distance and an_distance < (ap_distance + margin)
 
