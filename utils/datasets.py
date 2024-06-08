e47d6b5293399978150bd3e636d2e1ed7d9c16c3
raffaeleav <raff.aviello@gmail.com>
2024-06-08 21:51:20 +0200

    feat: fourier in notebook + df converter to fourier

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,7 +1,9 @@
 import os
+import numpy as np
 import pandas as pd
 
 from tqdm import tqdm
+from skimage import io, color, img_as_ubyte
 from pathlib import Path
 
 
@@ -74,10 +76,12 @@ def test(fake_dataset_path, real_dataset_path, df_out, output_dir):
     df_test_size = len(df_out) / 100 * 20
     df_test_size = int(df_test_size / 2)
 
+    # si ottengono le immagini già presenti nel dataset di training
     used_images = set(df_out["Anchor"].to_list() + df_out["Positive"].to_list() + df_out["Negative"].to_list())
     real_images = []
     fake_images = []
 
+    # f(n) = O(n), i set python sono hash table
     for i in tqdm(df_real["image_path"], desc="building (real column) test dataframe..."):
         if i not in used_images: 
             real_images.append(i)
@@ -97,8 +101,135 @@ def test(fake_dataset_path, real_dataset_path, df_out, output_dir):
     df_test.to_csv(output_dir, index=False)
 
 
+# funzione per convertire le immagini rgb nel dataset di training nello spettro di fourier
+def convert_train(fake_dataset_path, real_dataset_path, df_out): 
+    project_path = Path(__file__).parent.parent
+    fake_dataset = fake_dataset_path.split("\\")[-1]
+    real_dataset = real_dataset_path.split("\\")[-1]
+    temp_path = os.path.join(project_path, "temp")
+    output_path = os.path.join(temp_path, fake_dataset + "+" + real_dataset)
+
+    df_fourier = pd.DataFrame(columns=["Anchor", "Positive", "Negative"])
+
+    # controllo che non esistano le cartelle dove salvare le immagini, altrimenti le si creano
+    if not os.path.isdir(temp_path): 
+        os.mkdir(temp_path)
+    
+    if not os.path.isdir(output_path): 
+        os.mkdir(output_path)
+        os.mkdir(os.path.join(output_path, "train"))
+        os.mkdir(os.path.join(output_path, "train", fake_dataset))
+        os.mkdir(os.path.join(output_path, "train", real_dataset))
+
+    output_path = os.path.join(output_path, "train")
+
+    for index, row in tqdm(df_out.iterrows(), desc="converting rgb images to fourier spectrum..."):
+        a, p, n = row["Anchor"], row["Positive"], row["Negative"]
+
+        # si ottengono le immagini 
+        if str(row.Anchor).startswith("coco"):
+            a_img = io.imread(os.path.join(real_dataset_path, row.Anchor))
+            p_img = io.imread(os.path.join(real_dataset_path, row.Positive))
+            n_img = io.imread(os.path.join(fake_dataset_path, row.Negative))
+
+        else:
+            a_img = io.imread(os.path.join(fake_dataset_path, row.Anchor))
+            p_img = io.imread(os.path.join(fake_dataset_path, row.Positive))
+            n_img = io.imread(os.path.join(real_dataset_path, row.Negative))
+
+        # si convertono le immagini rgb in scala di grigi
+        a_img = color.rgb2gray(a_img)
+        p_img = color.rgb2gray(p_img)
+        n_img = color.rgb2gray(n_img)
+
+        # si effettua la trasformata di fourier
+        a_img = np.fft.fft2(a_img)
+        p_img = np.fft.fft2(p_img)
+        n_img = np.fft.fft2(n_img)
+
+        # si applica il logaritmo (ln) al valore assoluto dell'immagine per normalizzarla 
+        try: 
+            a_img = np.log(np.abs(a_img))
+            p_img = np.log(np.abs(p_img))
+            n_img = np.log(np.abs(n_img))
+        except Warning: 
+            # nelle immagini a basso contrasto il valore assoluto potrebbe essere zero
+            df_fourier.loc[index] = [
+                None,
+                None,
+                None
+            ]
+
+            pass 
+            continue
+                
+        # si considerano le frequenze necessarie
+        a_min = np.min(a_img)
+        a_max = np.max(a_img)
+        p_min = np.min(p_img)
+        p_max = np.max(p_img)
+        n_min = np.min(n_img)
+        n_max = np.max(n_img)
+
+        # controllo per evitare errori con le immagini con poco contrasto
+        if (a_max - a_min) <= 0:
+            a_img = (a_img - a_min) / ((a_max - a_min) + np.finfo(float).eps)
+        else: 
+            a_img = (a_img - a_min) / (a_max - a_min)
+
+        if (p_max - p_min) <= 0:
+            p_img = (p_img - p_min) / ((p_max - p_min) + np.finfo(float).eps)
+        else: 
+            p_img = (p_img - p_min) / (p_max - p_min)
+
+        if (n_max - n_min) <= 0:
+            n_img = (n_img - n_min) / ((n_max - n_min) + np.finfo(float).eps)
+        else: 
+            n_img = (n_img - n_min) / (n_max - n_min)
+
+        # si convertono le immagini in formato unsigned byte (al posto di fare / 255.0)
+        a_img = img_as_ubyte(a_img)
+        p_img = img_as_ubyte(p_img)
+        n_img = img_as_ubyte(n_img)
+
+        # si memorizzano le immagini fake in una cartella e quelle real in un'altra
+        if str(row.Anchor).startswith("coco"):
+            a_path = os.path.join(output_path, real_dataset, str(index) + "_anchor_real" + ".png")
+            p_path = os.path.join(output_path, real_dataset, str(index) + "_positive_real" + ".png")
+            n_path = os.path.join(output_path, fake_dataset, str(index) + "_negative_fake" + ".png")
+
+            # path da memorizzare nel df per caricare le immagini successivamente
+            df_a_path = str(index) + "_anchor_real" + ".png"
+            df_p_path = str(index) + "_positive_real" + ".png"
+            df_n_path = str(index) + "_negative_fake" + ".png"
+
+        else: 
+            a_path = os.path.join(output_path, fake_dataset, str(index) + "_anchor_fake" + ".png")
+            p_path = os.path.join(output_path, fake_dataset, str(index) + "_positive_fake" + ".png")
+            n_path = os.path.join(output_path, real_dataset, str(index) + "_negative_real" + ".png")
+
+            df_a_path = str(index) + "_anchor_fake" + ".png"
+            df_p_path = str(index) + "_positive_fake" + ".png"
+            df_n_path = str(index) + "_negative_real" + ".png"
+
+        io.imsave(a_path, a_img)
+        io.imsave(p_path, p_img)
+        io.imsave(n_path, n_img)
+
+        df_fourier.loc[index] = [
+            df_a_path,
+            df_p_path,
+            df_n_path
+        ]
+
+    df_fourier = df_fourier.dropna(how='any',axis=0) 
+    
+    output_dir = os.path.join(project_path, "datasets", "fourier_out.csv")
+    df_fourier.to_csv(output_dir, index=False)
+
+
 # attenzione ai path inseriti
-def build(dataset_size, fake_dataset, real_dataset):
+def build(fake_dataset, real_dataset, dataset_size):
     path = Path(__file__).parent.parent.parent
     project_path = Path(__file__).parent.parent
     artifact_path = os.path.join(path, "artifact")
@@ -106,13 +237,31 @@ def build(dataset_size, fake_dataset, real_dataset):
     real_dataset_path = os.path.join(artifact_path, real_dataset, "metadata.csv")
 
     output_dir = os.path.join(project_path, "datasets", "out.csv")
-    # train(fake_dataset_path, real_dataset_path, dataset_size, output_dir)
+    train(fake_dataset_path, real_dataset_path, dataset_size, output_dir)
 
-    df_out = pd.read_csv(output_dir)
+    # df_out = pd.read_csv(output_dir)
 
-    output_dir = os.path.join(project_path, "datasets", "testList.csv")
-    test(fake_dataset_path, real_dataset_path, df_out, output_dir)
+    # output_dir = os.path.join(project_path, "datasets", "testList.csv")
+    # test(fake_dataset_path, real_dataset_path, df_out, output_dir)
+
+
+def fourier(fake_dataset, real_dataset): 
+    path = Path(__file__).parent.parent.parent
+    project_path = Path(__file__).parent.parent
+    artifact_path = os.path.join(path, "artifact")
+    fake_dataset_path = os.path.join(artifact_path, fake_dataset)
+    real_dataset_path = os.path.join(artifact_path, real_dataset)
+    
+    df_out = pd.read_csv(os.path.join(project_path, "datasets", "out.csv"))
+    convert_train(fake_dataset_path, real_dataset_path, df_out)
+
+    # df_fourier = pd.read_csv(os.path.join(project_path, "datasets", "fourier.csv"))
 
 
 if __name__ == "__main__":
-    build(50000, "taming_transformer", "coco")
+    fake_dataset = "taming_transformer"
+    real_dataset = "coco"
+
+    # build(fake_dataset, real_dataset, 50000)
+
+    fourier(fake_dataset, real_dataset)
